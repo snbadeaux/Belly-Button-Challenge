a1671a9797c3a94f648cd8dbf00900f4da8d3e41
snbadeaux@gmail.com <snbadeaux@gmail.com>
2023-09-20 08:49:38 -0500

    adding minor changes

--- static/js/app.js
+++ static/js/app.js
@@ -88,10 +88,10 @@ function buildCharts(sample, data) {
           text: otu_labels.slice(0, 10),
       }];
 
-      let barlayout = [{
+      let barlayout = {
         title: "Top 10 Sample Values",
         titlefont: "Arial"
-      }];
+      };
   
   
       // Plot the bar chart using Plotly
@@ -139,11 +139,11 @@ function buildCharts(sample, data) {
               {range: [0, 1], color: "rgba(255, 255, 255, 0)"},
               {range: [1, 2], color: "rgba(232, 226, 202, .5)"},
               {range: [2, 3], color: "rgba(210, 206, 145, .5)"},
-              {range: [3, 4], color:  "rgba(202, 209, 95, .5)"},
-              {range: [4, 5], color:  "rgba(184, 205, 68, .5)"},
+              {range: [3, 4], color: "rgba(202, 209, 95, .5)"},
+              {range: [4, 5], color: "rgba(184, 205, 68, .5)"},
               {range: [5, 6], color: "rgba(170, 202, 42, .5)"},
               {range: [6, 7], color: "rgba(142, 178, 35 , .5)"},
-              {range: [7, 8], color:  "rgba(110, 154, 22, .5)"},
+              {range: [7, 8], color: "rgba(110, 154, 22, .5)"},
               {range: [8, 9], color: "rgba(50, 143, 10, 0.5)"},
               {range: [9, 10], color: "rgba(14, 127, 0, .5)"},
             ]
