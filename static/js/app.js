c33e569ffa0a64f60927b34d7ded1e8bafcbc6ce
snbadeaux@gmail.com <snbadeaux@gmail.com>
2023-09-20 08:47:04 -0500

    adding my try at bonus

--- static/js/app.js
+++ static/js/app.js
@@ -88,7 +88,10 @@ function buildCharts(sample, data) {
           text: otu_labels.slice(0, 10),
       }];
 
-      let barlayout = {title: "Top 10 Sample Values"}
+      let barlayout = [{
+        title: "Top 10 Sample Values",
+        titlefont: "Arial"
+      }];
   
   
       // Plot the bar chart using Plotly
@@ -114,6 +117,48 @@ function buildCharts(sample, data) {
   
       // Plot the bubble chart
       Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-    }
-  
+    
+
+
+    // Code for the Gauge Chart
+      let gaugedata = [
+        {
+          domain: { x: [0, 1], y: [0, 1] },
+          marker: {size: 28, color:'850000'},
+          value: wfreq,
+          title: {
+            text: "Belly Button Washing Frequency<br> Scrubs per Week",
+            font: { color: "black", size: 16}
+          },    
+          type: "indicator",
+          mode: "gauge+number",
+          gauge: {
+            axis: {range: [0,10], tickmode: "linear", tick0: 2, dtick: 2},
+            bar: {color: "black"},
+            steps: [
+              {range: [0, 1], color: "rgba(255, 255, 255, 0)"},
+              {range: [1, 2], color: "rgba(232, 226, 202, .5)"},
+              {range: [2, 3], color: "rgba(210, 206, 145, .5)"},
+              {range: [3, 4], color:  "rgba(202, 209, 95, .5)"},
+              {range: [4, 5], color:  "rgba(184, 205, 68, .5)"},
+              {range: [5, 6], color: "rgba(170, 202, 42, .5)"},
+              {range: [6, 7], color: "rgba(142, 178, 35 , .5)"},
+              {range: [7, 8], color:  "rgba(110, 154, 22, .5)"},
+              {range: [8, 9], color: "rgba(50, 143, 10, 0.5)"},
+              {range: [9, 10], color: "rgba(14, 127, 0, .5)"},
+            ]
+          }
+        }  
+      ];
+    
+      let gaugelayout = {
+          width: 600,
+          height: 450,
+          margin: { t: 100, r: 100, l: 100, b: 100 },
+          line: { color: '600000' },
+          font: { color: "#49a81d", family: "Arial, Helvetica, sans-serif" }
+       };
+
+       Plotly.newPlot("gauge", gaugedata, gaugelayout);
+    }       
         
